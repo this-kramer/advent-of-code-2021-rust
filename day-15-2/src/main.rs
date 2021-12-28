9c0c115e311703359c3b73559d99bab280c4f885
Paul Kramer <pkramer1503@gmail.com>
2021-12-28 23:15:09 +0100

    Add day 15 task 2

---
+++ day-15-2/src/main.rs
@@ -0,0 +1,161 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    println!(
+        "The best path has risk {}",
+        compute_cheapest_path("res/input.txt").unwrap()
+    );
+}
+
+fn compute_cheapest_path(path: &str) -> Option<u32> {
+    let mut field = read_input(path);
+    let target_position = (field.len() - 1, field[0].len() - 1);
+
+    let mut heap: BinaryHeap<State> = BinaryHeap::new();
+    let start_state = State {
+        cost: 0, // Best cost to direct neighbor
+        x: 0,
+        y: 0,
+    };
+
+    heap.push(start_state);
+    while let Some(frontier) = heap.pop() {
+        // Replace by heapify-up or similar?
+        let mut node = &mut field[frontier.y][frontier.x];
+
+        // There was a better route, note already finished.
+        if node.finished {
+            continue;
+        } else {
+            node.finished = true;
+        }
+
+        // Found target position
+        let cost = match (frontier.y, frontier.x) {
+            (0, 0) => 0,
+            (x, y) if (x, y) == target_position => return Some(frontier.cost + node.risk),
+            _ => frontier.cost + node.risk,
+        };
+
+        // Add non-finished neighbors to heap
+        if frontier.x > 0 {
+            heap.push(State {
+                cost: cost,
+                x: frontier.x - 1,
+                ..frontier
+            })
+        }
+        if frontier.x < target_position.1 {
+            heap.push(State {
+                cost: cost,
+                x: frontier.x + 1,
+                ..frontier
+            })
+        }
+        if frontier.y > 0 {
+            heap.push(State {
+                cost: cost,
+                y: frontier.y - 1,
+                ..frontier
+            })
+        }
+        if frontier.y < target_position.0 {
+            heap.push(State {
+                cost: cost,
+                y: frontier.y + 1,
+                ..frontier
+            })
+        }
+    }
+    None
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct State {
+    cost: u32,
+    x: usize,
+    y: usize,
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| other.y.cmp(&self.y))
+            .then_with(|| other.x.cmp(&self.x))
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Node {
+    risk: u32,
+    finished: bool,
+}
+
+fn read_input(path: &str) -> Vec<Vec<Node>> {
+    let field = read_input_small(path);
+    let mut large_field: Vec<Vec<Node>> = Vec::new();
+
+    for i in 0..5 {
+        for row in &field {
+            large_field.push(
+                row.iter()
+                    .map(|&x| (x - 1 + i) % 9 + 1)
+                    .chain(row.iter().map(|&x| (x + i) % 9 + 1))
+                    .chain(row.iter().map(|&x| (x + 1 + i) % 9 + 1))
+                    .chain(row.iter().map(|&x| (x + 2 + i) % 9 + 1))
+                    .chain(row.iter().map(|&x| (x + 3 + i) % 9 + 1))
+                    .map(|x| Node {
+                        risk: x,
+                        finished: false,
+                    })
+                    .collect(),
+            );
+        }
+    }
+
+    large_field
+}
+
+fn read_input_small(path: &str) -> Vec<Vec<u32>> {
+    let file = File::open(path).expect("Error opening file");
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+
+    let mut field = Vec::new();
+    for line in lines {
+        field.push(
+            line.expect("Error parsing line")
+                .chars()
+                .map(|x| x.to_digit(10).unwrap())
+                .collect(),
+        );
+    }
+    field
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let _ = read_input("res/test-input.txt");
+    }
+
+    #[test]
+    fn test_dijkstra() {
+        assert_eq!(Some(315), compute_cheapest_path("res/test-input.txt"));
+    }
+}
